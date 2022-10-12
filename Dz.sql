c191da8f14bd26f4aef5895a9b53e761f0f097ba
~Andrey Shabalin~ <59790429+Andreyshabalinn@users.noreply.github.com>
2022-10-12 15:51:33 +0600

    Пофиксил названия таблиц
    Подозреваю проблема была в заглавных/строчных буквах, поменял
    Затрудняюсь назвать истинную причину, потому что у меня до фикса работало

--- Dz.sql
+++ Dz.sql
@@ -60,11 +60,11 @@ insert into Positions (title, description,price,partner_id) values
  ("Раф медовый","Эспрессо со сливками и мёдом","1200",(select id from Partners where title = "Angel In Us")),
  ("Матча чай","Японский порошковый чай","1000",(select id from Partners where title = "Angel In Us")),
  ("Тирамису","Итальянское пирожное","2000",(select id from Partners where title = "Angel In Us"));
- insert into clients (phone,fullname) values
+ insert into Clients (phone,fullname) values
  ("+78005553535","Андрей Шабалин"),
  ("+77072119034","Gyro Zeppeli"),
  ("+77714010218","Ертай Сарбасов");
- insert into orders (created_at,address,status,client_id) values
+ insert into Orders (created_at,address,status,client_id) values
 (SYSDATE(),"Nazarbayev, 228","new",(select id from Clients where fullname = "Андрей Шабалин")),
 (SYSDATE(),"Abay 44, ","accepted",(select id from Clients where fullname = "Андрей Шабалин")),
 (SYSDATE(),"Al-Farabi, 140","in progress",(select id from Clients where fullname = "Gyro Zeppeli")),
@@ -72,19 +72,21 @@ insert into Positions (title, description,price,partner_id) values
 (SYSDATE(),"Zhibek-Zholy, 70","done",(select id from Clients where fullname = "Ертай Сарбасов")),
 (SYSDATE(),"Tole-bi, 10","in progress",(select id from Clients where fullname = "Ертай Сарбасов"));
  insert into position_orders(order_id,position_id) values
-((select id from Orders where id = 1),(select id from positions where title = "Тирамису")),
-((select id from Orders where id = 1),(select id from positions where title = "Раф медовый")),
-((select id from Orders where id = 1),(select id from positions where title = "Панини")),
-((select id from Orders where id = 2),(select id from positions where title = "Панини")),
-((select id from Orders where id = 2),(select id from positions where title = "Облепиховый чай")),
-((select id from Orders where id = 3),(select id from positions where title = "Цезарь с курицей")),
-((select id from Orders where id = 3),(select id from positions where title = "Матча чай")),
-((select id from Orders where id = 3),(select id from positions where title = "Тирамису")),
-((select id from Orders where id = 4),(select id from positions where title = "Матча чай")),
-((select id from Orders where id = 4),(select id from positions where title = "BigMac")),
-((select id from Orders where id = 5),(select id from positions where title = "Fries")),
-((select id from Orders where id = 5),(select id from positions where title = "Royal Cheesburger")),
-((select id from Orders where id = 6),(select id from positions where title = "Цезарь с курицей"));
+((select id from Orders where id = 1),(select id from Positions where title = "Тирамису")),
+((select id from Orders where id = 1),(select id from Positions where title = "Раф медовый")),
+((select id from Orders where id = 1),(select id from Positions where title = "Панини")),
+((select id from Orders where id = 2),(select id from Positions where title = "Панини")),
+((select id from Orders where id = 2),(select id from Positions where title = "Облепиховый чай")),
+((select id from Orders where id = 3),(select id from Positions where title = "Цезарь с курицей")),
+((select id from Orders where id = 3),(select id from Positions where title = "Матча чай")),
+((select id from Orders where id = 3),(select id from Positions where title = "Тирамису")),
+((select id from Orders where id = 4),(select id from Positions where title = "Матча чай")),
+((select id from Orders where id = 4),(select id from Positions where title = "BigMac")),
+((select id from Orders where id = 5),(select id from Positions where title = "Fries")),
+((select id from Orders where id = 5),(select id from Positions where title = "Royal Cheesburger")),
+((select id from Orders where id = 6),(select id from Positions where title = "Цезарь с курицей"));
+
+
 /*
 Задание 2
 Запрос вывода номера заказов, телефона клиентов и названия партнеров
@@ -93,11 +95,11 @@ SELECT
  	Orders.id as "Номер заказа", 
  	Clients.phone as "Телефон клиента", 
  	Partners.title as "Партнёр"
-from Orders,clients,position_orders,positions,partners
+from Orders,Clients,position_orders,Positions,partners
 where Orders.client_id = Clients.id
 and Orders.id = position_orders.order_id
 and Positions.id = position_orders.position_id
-and Partners.id = positions.partner_id;
+and Partners.id = Positions.partner_id;
 /*Можно ещё через джоины, но через where мне кажется по красивее*/
 SELECT
  	Orders.id as "Номер заказа",
@@ -108,10 +110,10 @@ join position_orders
 on Orders.id = position_orders.order_id
 join Positions 
 on Positions.id = position_orders.position_id
-join clients
+join Clients
 on Orders.client_id = Clients.id
 join partners
-on Partners.id = positions.partner_id;
+on Partners.id = Positions.partner_id;
 /*
  Задание 3
  Создаю позицию с пустым заказом и вывожу его
@@ -122,13 +124,13 @@ insert into Positions (title, description,price,partner_id) values
  ("Tasty Delicios Burger","suppose to be meat,salad, tomato,","2000",(select id from Partners where title = "Bahandi"));
  select * from Partners
  where partners.id not in 
- (select partner_id from position_orders, positions where Positions.id = position_orders.position_id);
+ (select partner_id from position_orders, Positions where Positions.id = position_orders.position_id);
  /*
   Задание 4
   Вывести названия всех позиций заказа 
   */
-  select positions.title from orders, positions, position_orders
+  select Positions.title from Orders, Positions, position_orders
  	where position_orders.order_id = Orders.id
- 	and position_orders.position_id = positions.id 
- 	and orders.id = 20 /*Ввести id заказа*/
- 	and orders.client_id = 1; /*Ввести id клиента*/
+ 	and position_orders.position_id = Positions.id 
+ 	and Orders.id = 20 /*Ввести id заказа*/
+ 	and Orders.client_id = 1; /*Ввести id клиента*/
